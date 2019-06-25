ad7d76d820924d2ef3839fa2ee8531f14e7b8058
kein <kein110@mail.ru>
2019-06-25 15:09:24 +0500

    learning page transitions

--- pages/locators.py
+++ pages/locators.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators(object):
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_invalid")
 	
 class LoginPageLocators(object):
 	#LOGIN_url = (By.CSS_SELECTOR, "login")
